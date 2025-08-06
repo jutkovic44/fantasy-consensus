d0fa1061354c5345d8aeccb56413eb80210e510b
jutkovic44 <jamesutkovic@gmail.com>
2025-08-05 22:19:36 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 document.getElementById('startDraft').onclick = () => {
   document.getElementById('introOverlay').style.display = 'none';
+  document.getElementById('app').style.display = 'block';
   document.getElementById('pauseResume').style.display = 'inline-block';
-  console.log("Draft started. Rankings revealed.");
-  // You'd normally call setupDraft() or similar here.
+  console.log("Draft started.");
 };
